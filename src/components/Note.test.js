17e8a7ad954e2b731a88a2208d7cfd6e9c186e8f
gps2601 <smith260194@gmail.com>
2019-09-16 21:33:32 +0100

    Adding test to check clicking button fires event handler using a mock

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import Note from './note'
-
-afterEach(cleanup)
 
 test('renders content', () => {
   const note = {
@@ -18,4 +16,22 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
+})
+
+test('clicking the button calls event handler once', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+
+  const button = getByText('make not important')
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(1)
 })
